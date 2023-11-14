a1c418d53a923b7600b02fb2f34018cbc43722fc
RohitM-IN <rohit.mahajan.in@gmail.com>
2023-11-14 21:57:16 +0530

    Added Configuration contract

---
+++ Sync.Core/DataContract/Config/GlobalSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sync.Core.DataContract.Config
+{
+    /// <summary>
+    /// Note: Key columns and excluded columns are used for identification and exclusion respectively,
+    /// but only if the specified columns are present in a table. Non-existent columns will be ignored
+    /// during synchronization.
+    /// </summary>
+    public class GlobalSettings
+    {
+        /// <summary>
+        /// Gets or sets the list of key columns used for identification in synchronization across all tables.
+        /// </summary>
+        public List<string> KeyColumns { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of columns excluded from synchronization across all tables.
+        /// </summary>
+        public List<string> ExcludedColumns { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether private properties should be excluded from synchronization.
+        /// Default is true.
+        /// </summary>
+        public bool ExcludePrivateProperties { get; set; } = true;
+    }
+}
---
+++ Sync.Core/DataContract/Config/SyncConfiguation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sync.Core.DataContract.Config
+{
+    /// <summary>
+    /// Represents the configuration settings for synchronization, including global settings and table-specific settings.
+    /// </summary>
+    public class SyncConfiguation
+    {
+        /// <summary>
+        /// Gets or sets the global synchronization settings.
+        /// </summary>
+        public GlobalSettings Global { get; set; }
+
+        /// <summary>
+        /// Gets or sets the table-specific synchronization settings, organized as a dictionary where the key is the table name.
+        /// </summary>
+        public Dictionary<string, TableSettings> Tables { get; set; }
+
+        /// <summary>
+        /// Gets the list of excluded columns for a specific table, combining both table-specific and global exclusions.
+        /// </summary>
+        /// <param name="tableName">The name of the table.</param>
+        /// <returns>A list of excluded column names.</returns>
+        public List<string> GetExcludedList(string tableName)
+        {
+            TableSettings settings;
+            var excluded = new List<string>();
+            Tables.TryGetValue(tableName, out settings);
+            {
+                excluded.AddRange(settings.ExcludedColumns);
+            }
+            excluded.AddRange(Global.ExcludedColumns);
+
+            return excluded;
+        }
+
+        /// <summary>
+        /// Gets the list of key columns for a specific table, combining both table-specific and global key columns.
+        /// </summary>
+        /// <param name="tableName">The name of the table.</param>
+        /// <returns>A list of key column names.</returns>
+        public List<string> GetKeyColumnsList(string tableName)
+        {
+            TableSettings settings;
+            var keyColumns = new List<string>();
+            Tables.TryGetValue(tableName, out settings);
+            {
+                keyColumns.AddRange(settings.KeyColumns);
+            }
+            keyColumns.AddRange(Global.KeyColumns);
+
+            return keyColumns;
+        }
+    }
+}
