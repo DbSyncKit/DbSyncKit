e378473a93c39bd65fbda73639d9c95fc566df48
RohitM-IN <rohit.mahajan.in@gmail.com>
2023-11-14 23:46:00 +0530

    added keyEqualityComparer & DataMetadataComparison classes

---
+++ Sync.Core/Comparer/KeyEqualityComparer.cs
@@ -0,0 +1,62 @@
+﻿using Sync.DB.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sync.Core.Comparer
+{
+    public class KeyEqualityComparer<T> : IEqualityComparer<T> where T : DataContractUtility<T>
+    {
+        private readonly PropertyInfo keyProperty;
+
+        private readonly List<string> keyColumns;
+        private readonly List<string> ignoredColumns;
+
+        public KeyEqualityComparer(List<string> keyColumns, List<string> ignoredColumns)
+        {
+            keyProperty = typeof(T).GetProperties()
+                .FirstOrDefault(prop => Attribute.IsDefined(prop, typeof(KeyAttribute)))!;
+
+            if (keyProperty == null)
+            {
+                throw new InvalidOperationException($"No property with KeyAttribute found in type {typeof(T).Name}");
+            }
+            if (!keyColumns.Contains(keyProperty.Name))
+                keyColumns.Add(keyProperty.Name);
+
+            this.keyColumns = keyColumns;
+            this.ignoredColumns = ignoredColumns;
+        }
+
+
+        public bool Equals(T x, T y)
+        {
+            var properties = typeof(T).GetProperties()
+                .Where(prop => keyColumns.Contains(prop.Name) && !ignoredColumns.Contains(prop.Name));
+
+            return properties.All(prop => Equals(prop.GetValue(x), prop.GetValue(y)));
+        }
+
+        public int GetHashCode(T obj)
+        {
+            unchecked
+            {
+                int hash = 17;
+                var properties = typeof(T).GetProperties()
+                    .Where(prop => keyColumns.Contains(prop.Name) && !ignoredColumns.Contains(prop.Name));
+
+                foreach (var prop in properties)
+                {
+                    var value = prop.GetValue(obj);
+                    hash = hash ^ ((value?.GetHashCode() ?? 0) + 23);
+                }
+
+                return hash;
+            }
+        }
+    }
+}
---
+++ Sync.Core/Helper/DataMetadataComparisonHelper.cs
@@ -0,0 +1,102 @@
+﻿using Sync.Core.Comparer;
+using Sync.Core.DataContract;
+using Sync.DB.Utils;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sync.Core.Helper
+{
+    public class DataMetadataComparisonHelper<T> where T : DataContractUtility<T>
+    {
+        public static Result<T> GetDifferences(HashSet<T> sourceList, HashSet<T> destinationList, List<string> keyProperties, List<string> excludedProperties)
+        {
+            List<T> added = new List<T>();
+            List<T> deleted = new List<T>();
+            ConcurrentBag<(T edit, Dictionary<string, object> updatedProperties)> edited = new ConcurrentBag<(T, Dictionary<string, object>)>();
+
+            var keyComparer = new KeyEqualityComparer<T>(keyProperties, excludedProperties);
+            var properties = typeof(T).GetProperties()
+                .Where(prop => keyProperties.Contains(prop.Name) && !excludedProperties.Contains(prop.Name));
+
+            //Columns.Add(property.Name);
+
+            // Identify added entries
+            added.AddRange(destinationList.Except(sourceList, keyComparer));
+
+            // Identify deleted entries
+            deleted.AddRange(sourceList.Except(destinationList, keyComparer));
+
+            // Identify edited entries
+            var sourceKeyDictionary = sourceList
+                .Except(added)
+                .ToDictionary(row => GenerateCompositeKey(row, keyProperties), row => row);
+
+            var destinationKeyDictionary = destinationList
+                .Except(deleted)
+                .ToDictionary(row => GenerateCompositeKey(row, keyProperties), row => row);
+
+            Parallel.ForEach(sourceKeyDictionary, kvp =>
+            {
+                var sourceContract = kvp.Value;
+
+                T destinationContract;
+                if (destinationKeyDictionary.TryGetValue(GenerateCompositeKey(sourceContract, keyProperties), out destinationContract))
+                {
+                    var (isEdited, updatedProperties) = GetEdited(sourceContract, destinationContract, excludedProperties);
+
+                    if (isEdited)
+                    {
+                        edited.Add((sourceContract, updatedProperties));
+                    }
+                }
+            });
+
+
+            var result = new Result<T>();
+            result.Added = added;
+            result.Deleted = deleted;
+            result.Edited = edited.ToList();
+
+            return result;
+        }
+
+        private static (bool isEdited, Dictionary<string, object> updatedProperties) GetEdited(T source, T destination, List<string> excludedProperties)
+        {
+            Dictionary<string, object> updatedProperties = new Dictionary<string, object>();
+            bool isEdited = false;
+            if (source.Equals(destination, excludedProperties))
+            {
+                return (isEdited, updatedProperties);
+            }
+
+            foreach (PropertyInfo prop in typeof(T).GetProperties().Where(prop => !excludedProperties.Contains(prop.Name)))
+            {
+                object sourceValue = prop.GetValue(source);
+                object destinationValue = prop.GetValue(destination);
+
+                // Compare values
+                if (!EqualityComparer<object>.Default.Equals(sourceValue, destinationValue))
+                {
+                    isEdited = true;
+                    updatedProperties[prop.Name] = sourceValue;
+                }
+            }
+
+            return (isEdited, updatedProperties);
+        }
+
+        // Function to generate a composite key based on specified properties
+        private static string GenerateCompositeKey(T DataContract, List<string> keyProperties)
+        {
+            // Concatenate property values based on keyProperties
+            var compositeKey = string.Join("_", keyProperties.Select(propName => DataContract.GetType().GetProperty(propName)?.GetValue(DataContract)));
+            return compositeKey;
+        }
+
+    }
+}
