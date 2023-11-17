c273e4b2601869cc96004d0ff0f9881521b97c57
RohitM-IN <rohit.mahajan.in@gmail.com>
2023-11-17 19:26:34 +0530

    Added GenerateInsertWithIDAttribute

---
+++ Sync.DB/Attributes/GenerateInsertWithIDAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sync.DB.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class GenerateInsertWithIDAttribute : Attribute
+    {
+        // if insert querry generation will be with id or without id
+    }
+}
