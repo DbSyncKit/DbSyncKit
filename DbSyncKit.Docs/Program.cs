82516da6b9df88d3541c3da0f0ca2a7a5085a21e
RohitM-IN <rohit.mahajan.in@gmail.com>
2023-12-15 02:12:03 +0530

    Added Docs

---
+++ DbSyncKit.Docs/Program.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Statiq.Markdown;
+
+namespace DbSyncKit.ConsoleApp
+{
+    internal class Program
+    {
+        public static async Task<int> Main(string[] args) 
+        {
+            var enviroment = System.Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(enviroment).FullName;
+
+            return await Bootstrapper.Factory
+            .CreateDocs(args)
+            .RunAsync();
+        }
+    }
+}
